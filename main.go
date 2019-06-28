88bce56b5e6ea08ef9341d8aa4dfe1dd9aeccc75
Tushar Jois <tushar.jois@jhu.edu>
2019-06-27 19:19:00 -0500

    fixed endianness

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	tftp.CreateWritePacket("hello.txt")
+	tftp.ListenForFile()
 }
--- tftp/tftp.go
+++ tftp/tftp.go
@@ -6,10 +6,20 @@ import (
 	"fmt"
 )
 
+type tftpError uint8
+
+const (
+	errUndef tftpError = iota
+	errNotFound
+	errAccess
+	errNoSpace
+	errIllegalOp
+)
+
 func createPacket(data []interface{}) *bytes.Buffer {
 	pkt := new(bytes.Buffer)
 	for _, v := range data {
-		err := binary.Write(pkt, binary.LittleEndian, v)
+		err := binary.Write(pkt, binary.BigEndian, v)
 		if err != nil {
 			fmt.Println("binary.Write failed:", err)
 		}
@@ -17,16 +27,41 @@ func createPacket(data []interface{}) *bytes.Buffer {
 	return pkt
 }
 
-func CreateWritePacket(filename string) *bytes.Buffer {
+func createWriteRequest(filename string) *bytes.Buffer {
 	var data = []interface{}{
-		uint8(0),
-		uint8(2),         // opcode (WRQ)
+		uint16(2),        // opcode (WRQ)
 		[]byte(filename), // filename
 		uint8(0),         // NUL
 		[]byte("octet"),  // mode
 		uint8(0),         // NUL
 	}
 	pkt := createPacket(data)
-	fmt.Printf("% x\n", pkt.Bytes())
 	return pkt
+}
+
+func createAck(blockNum uint16) *bytes.Buffer {
+	var data = []interface{}{
+		uint16(4),        // opcode (ACK)
+		uint16(blockNum), // block number
+	}
+	pkt := createPacket(data)
+	return pkt
+}
+
+func createError(e tftpError) *bytes.Buffer {
+	var data = []interface{}{
+		uint16(5), // opcode (ERR)
+		uint8(e),  // error number
+		uint8(0),  // NUL
+	}
+	pkt := createPacket(data)
+	return pkt
+}
+
+func parsePacket(pkt *bytes.Buffer) {
+
+}
+
+func ListenForFile() {
+	fmt.Printf("% x\n", createWriteRequest("rfc1350.txt"))
 }
