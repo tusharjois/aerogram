284e1a0f532bc5270f2348372e0275a4051f8234
Tushar Jois <16180220+tusharjois@users.noreply.github.com>
2019-08-01 18:21:04 -0500

    code cleanup

--- main.go
+++ main.go
@@ -13,14 +13,11 @@ import (
 )
 
 func main() {
-	// TODO: actual command line flags
-	var isServer = flag.Bool("recv", false, "receive a file")
-	var isClient = flag.Bool("send", false, "send a file")
-	var inFile = flag.String("infile", "", "filename to send, leave blank for stdin")
-	var outFile = flag.String("outfile", "", "filename to save as, leave blank for stdout")
-	var isDebug = flag.Bool("debug", false, "show debug log")
+	var inFile = flag.String("sendfile", "", "filename to send, leave blank for stdin")
+	var outFile = flag.String("recvfile", "", "filename to save as, leave blank for stdout")
 	var useGzip = flag.Bool("gzip", false, "compress/decompress with gzip")
-	// var timeoutFlag = flag.Duration("timeout", time.Minute, "amount of time to wait for a connection")
+	var isDebug = flag.Bool("debug", false, "show debug log")
+	// TODO: "secure" flag
 	flag.Parse()
 
 	if !*isDebug {
@@ -28,22 +25,38 @@ func main() {
 		log.SetOutput(ioutil.Discard)
 	}
 
-	if *isServer {
+	st, _ := os.Stdin.Stat()
+	isStdin := st.Size() > 0
+
+	if !isStdin || *outFile != "" {
+		// Run in server mode.
 		ln, err := net.Listen("tcp", ":26465")
 		if err != nil {
-			// TODO handle error
+			log.Printf("[ERR] server: %v\n", err)
+			fmt.Fprintf(os.Stderr, "cannot listen for aerogram on %v\n", "26465")
+			os.Exit(1)
 		}
 
-		// Setup our service export
+		// Setup our service export.
 		host, _ := os.Hostname()
 		info := []string{"simple 1:1 filesharing over a local network"}
 
-		ifaces, _ := net.Interfaces()
+		ifaces, err := net.Interfaces()
+		if err != nil {
+			log.Printf("[ERR] server: %v\n", err)
+			fmt.Fprintln(os.Stderr, "error: cannot load network interfaces")
+			os.Exit(1)
+		}
+
+		// Get the list of addresses on which to advertise.
 		var ips []net.IP
-		// TODO: handle err
 		for _, i := range ifaces {
 			addrs, _ := i.Addrs()
-			// TODO: handle err
+			if err != nil {
+				log.Printf("[ERR] server: %v\n", err)
+				fmt.Fprintf(os.Stderr, "error: cannot load addresses for interface %v\n", i.Name)
+				os.Exit(1)
+			}
 			for _, addr := range addrs {
 				var ip net.IP
 				switch v := addr.(type) {
@@ -52,33 +65,37 @@ func main() {
 				case *net.IPAddr:
 					ip = v.IP
 				}
-				// process IP address
 				ips = append(ips, ip)
 			}
 		}
 
+		// Stand up the mDNS receiver.
 		service, _ := mdns.NewMDNSService(host, "_aerogram._tcp.", "", "", 26465, ips, info)
 		server, _ := mdns.NewServer(&mdns.Config{Zone: service})
+		// TODO: Handle Shutdown in the os.Exit(1) case.
 		defer server.Shutdown()
 
-		for {
-			conn, err := ln.Accept()
-			if err != nil {
-				// TODO handle error
-			}
-			//go func() {
-			err = transfer.ReceiveAerogram(conn, *outFile, *useGzip)
-			if err != nil {
-				log.Fatalf("[ERR] server: %v\n", err)
-			}
-			//}()
-			return
+		// Accept a send request.
+		// TODO: How would this look with more than one connection?
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("[ERR] server: %v\n", err)
+			fmt.Fprintln(os.Stderr, "error: cannot accept connection")
+			os.Exit(1)
 		}
-	} else if *isClient {
-		// Make a channel for results and start listening
-		// TODO: allow for more than 1
-		entriesCh := make(chan *mdns.ServiceEntry, 1)
 
+		// Receive the aerogram.
+		err = transfer.ReceiveAerogram(conn, *outFile, *useGzip)
+		if err != nil {
+			log.Printf("[ERR] server: %v\n", err)
+			fmt.Fprintf(os.Stderr, "error: cannot receive aerogram from %v\n", conn.RemoteAddr().String())
+			os.Exit(1)
+		}
+	} else {
+		// Run in client mode.
+		// Make a channel for results and start listening.
+		// TODO: Allow for more than 1 entry.
+		entriesCh := make(chan *mdns.ServiceEntry, 1)
 		go func() {
 			// Start the lookup
 			qParams := mdns.DefaultParams("_aerogram._tcp.")
@@ -90,23 +107,26 @@ func main() {
 
 		entry, present := <-entriesCh
 		if !present {
-			log.Fatalln("[WARN] aerogram: request timed out")
+			fmt.Fprintln(os.Stderr, "request timed out")
+			os.Exit(1)
 		}
-		fmt.Println(entry)
+		log.Printf("[INFO] client: found entry %v\n", entry)
 		connString := fmt.Sprintf("%s:%d", entry.AddrV4.String(), entry.Port)
+
+		// Connect to the aerogram instance.
 		conn, err := net.Dial("tcp", connString)
 		if err != nil {
-			// handle error
-			log.Printf("[ERR] client: ", err)
-			log.Fatalf("[ERR] client: cannot connect to %v\n", connString)
+			log.Printf("[ERR] client: %v\n", err)
+			fmt.Fprintf(os.Stderr, "error: cannot connect to %v\n", connString)
+			os.Exit(1)
 		}
-		transfer.SendAerogram(conn, *inFile, *useGzip)
-		// err := tftp.WriteFileToServer(*inFile, connString)
-		// if err != nil {
-		// 	log.Fatalf("[ERR] client: %s\n", err)
-		// }
-	} else {
-		fmt.Fprintln(os.Stderr, "please specify --send or --recv")
-		os.Exit(1)
+
+		// Send the aerogram.
+		err = transfer.SendAerogram(conn, *inFile, *useGzip)
+		if err != nil {
+			log.Printf("[ERR] client: %v\n", err)
+			fmt.Fprintf(os.Stderr, "error: cannot send aerogram to %v\n", connString)
+			os.Exit(1)
+		}
 	}
 }
